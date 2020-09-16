64e763f5acbb377c8ef02108d5bb75c2ee33c574
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-09-16 21:53:26 +0300

    Add files via upload

--- SciPyProjects/PhasePortrait/ThirdSemTasks/DigitalSignalProcessing/FourierProcessing.py
+++ SciPyProjects/PhasePortrait/ThirdSemTasks/DigitalSignalProcessing/FourierProcessing.py
@@ -2,12 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-
 def fourier_first(x: float, steps_n: int = 10):
     sums = 0
 
     for i in range(1, steps_n):
-        sums += ((-1) ** (i + 1)) * np.sin(i*x) / i
+        sums += ((-1) ** (i + 1)) * np.sin(i*x) / i ** 2
 
     return 2 - sums
 
@@ -31,7 +30,7 @@ def fourier_third(x: float, steps_n: int = 10):
     return 4 / np.pi * sums
 
 
-def render_graphing_fourier(foo, interval_start, interval_end):
+def render_graphing_fourier(foo, real_foo, interval_start, interval_end):
     x = np.linspace(interval_start, interval_end)
 
     y = foo(x, steps_n=3)
@@ -43,11 +42,14 @@ def render_graphing_fourier(foo, interval_start, interval_end):
     y = foo(x, steps_n=50)
     plt.plot(x, y, color='g', label='steps=50')
 
+    y = real_foo(x)
+    plt.plot(x, y, color='black', label='func')
+
     plt.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    render_graphing_fourier(fourier_first, -np.pi, np.pi)
-    render_graphing_fourier(fourier_second, -np.pi, np.pi)
-    render_graphing_fourier(fourier_third, -np.pi, np.pi)
+    render_graphing_fourier(fourier_first, lambda x: x, -np.pi, np.pi)
+    render_graphing_fourier(fourier_second, lambda x: x*x, -np.pi, np.pi)
+    render_graphing_fourier(fourier_third, lambda x: np.sign(x), -np.pi, np.pi)
