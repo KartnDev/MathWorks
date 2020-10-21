f52eb0a04dad05d7cd75f6cdf4d05ee13d6e925a
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-10-21 23:44:52 +0300

    update task3

--- SciPyProjects/FifthSemTasks/NeuralInterfaces/dataSignal3.py
+++ SciPyProjects/FifthSemTasks/NeuralInterfaces/dataSignal3.py
@@ -1,50 +1,85 @@
-import asyncio
-
+import socket
 import numpy as np
-import scipy
-import pandas as pd
 import matplotlib.pyplot as plt
+from mne.time_frequency import morlet
 
-from NeuralInterfaces.dataSignal2 import read_signals
+from NeuralInterfaces.dataSignal import read_signals
 
-
-async def yield_signal(src_signal_open, src_signal_close):
-    while True:
-        new_sig_open = src_signal_open + 0.0
-        new_sig_close = src_signal_close + 0.0
-
-        batch_size = 50
-        rand_index = np.random.randint(0, 105 - batch_size)
-
-        open_batch = new_sig_open[rand_index: rand_index + batch_size]
-        close_batch = new_sig_close[rand_index: rand_index + batch_size]
-
-        normal_emit_open = 1
-        normal_emit_close = 1
-
-        open_batch += np.random.normal(0, normal_emit_open, open_batch.shape)
-        close_batch += np.random.normal(0, normal_emit_close, close_batch.shape)
-        for sig_open, sig_close in zip(open_batch, close_batch):
-            yield sig_open, sig_close
-            await asyncio.sleep(0.01)
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 1337  # The port used by the server
 
 
-async def main():
-    o_signal_first, o_signal_second, o_signal_third = read_signals("Resource\\OpenEyes.asc")
-    c_signal_first, c_signal_second, c_signal_third = read_signals("Resource\\ClosedEyes.asc")
-    arr_open = []
-    arr_close = []
-    async for sig_open, sig_close in yield_signal(o_signal_second, c_signal_second):
-        arr_open.append(sig_open)
-        arr_close.append(sig_close)
-        if len(arr_open) > 50:
-            plt.plot(np.abs(np.fft.rfft(arr_open)) ** 2, label='open')
-            plt.plot(np.abs(np.fft.rfft(arr_close)) ** 2, label='close')
-            plt.legend()
-            plt.show()
-            arr_open = []
-            arr_close = []
+def inf_list_draw(data, fig, sample):
+    fig.canvas.draw()
+    fig.clf()
+    plt.ylim(bottom=0, top=1e8)
+    plt.plot(np.abs(np.fft.fft(data)) ** 2, color='green')
+    plt.plot(sample, color='red')
+
+
+def inf_draw(data, fig):
+    fig.canvas.draw()
+    fig.clf()
+    plt.plot(data, color='green')
+
+
+def inf2(data, fig):
+    fig.canvas.draw()
+    fig.clf()
+    sf = 36
+    cf = 13  # Central spindles frequency in Hz
+    nc = 12  # Number of oscillations in the spindles
+    times = np.arange(data.size) / sf
+    wlt = morlet(sf, [cf], n_cycles=nc)[0]
+    analytic = np.convolve(data, wlt, mode='same')
+    magnitude = np.abs(analytic)
+    phase = np.angle(analytic)
+
+    # Square and normalize the magnitude from 0 to 1 (using the min and max)
+    power = np.square(magnitude)
+    norm_power = (power - power.min()) / (power.max() - power.min())
+
+    # Define the threshold
+    thresh = 0.25
+
+    # Find supra-threshold values
+    supra_thresh = np.where(norm_power >= thresh)[0]
+
+    # Create vector for plotting purposes
+    val_spindles = np.nan * np.zeros(data.size)
+    val_spindles[supra_thresh] = data[supra_thresh]
+
+    plt.plot(times, norm_power)
+    #plt.set_xlabel('Time [sec]')
+    #plt.set_ylabel('Normalized wavelet power')
+    #plt.axhline(thresh, ls='--', color='indianred', label='Threshold')
+    plt.fill_between(times, norm_power, thresh, where=norm_power >= thresh,
+                     color='indianred', alpha=.8)
+    #plt.legend(loc='best')
+
+
+def main():
+    sigFirst = read_signals("Resource\\ClosedEyes.asc")[0][3:103]
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
+        fig = plt.gcf()
+        fig.show()
+        fig.canvas.draw()
+
+        data = list(s.recv(1024)[1:100])
+        # plt.plot(data)
+
+        plt.ion()  # enable interactivity
+
+        while True:
+            new = list(s.recv(202)[1:100])
+            for i in new:
+                data.append(i)
+                inf2(np.array(data[-100:]), fig)
+                #inf_list_draw(data[-100:], fig, sigFirst)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
