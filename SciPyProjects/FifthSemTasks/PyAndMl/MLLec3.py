4af9b02db920dd220a1f5988d35468cb84225e1d
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-10-27 06:04:07 -0700

    Update MLLec3.py

--- SciPyProjects/FifthSemTasks/PyAndMl/MLLec3.py
+++ SciPyProjects/FifthSemTasks/PyAndMl/MLLec3.py
@@ -7,29 +7,39 @@ def full_circle_shift_clockwise(matrix_array: np.array, index_of_circle: int):
     # init indexes
     last_index = 1
 
-
     # shift first row (init first and slice other)
     matrix_array[index_of_circle, index_of_circle] = not_shifted[index_of_circle + 1, index_of_circle]
 
-    for i in range(index_of_circle + 1, matrix_array.shape[0] - index_of_circle):
+    for i in range(index_of_circle + 1, matrix_array.shape[1] - index_of_circle):
         matrix_array[index_of_circle, i] = not_shifted[index_of_circle, i - 1]
 
     # shift last column
-    for i in range(index_of_circle + 1, matrix_array.shape[1] - index_of_circle):
-        matrix_array[i, matrix_array.shape[0] - index_of_circle - 1] = not_shifted[i - 1, matrix_array.shape[0] - index_of_circle - 1]
+    for i in range(index_of_circle + 1, matrix_array.shape[0] - index_of_circle):
+        matrix_array[i, matrix_array.shape[1] - index_of_circle - 1] = not_shifted[
+            i - 1, matrix_array.shape[1] - index_of_circle - 1]
 
     # shift last row
     for i in reversed(range(index_of_circle, matrix_array.shape[1] - index_of_circle - 1)):
-        matrix_array[matrix_array.shape[1] - index_of_circle - 1, i] = not_shifted[matrix_array.shape[0] - index_of_circle - 1, i + 1]
+        matrix_array[matrix_array.shape[0] - index_of_circle - 1, i] = not_shifted[
+            matrix_array.shape[0] - index_of_circle - 1, i + 1]
 
     for i in reversed(range(index_of_circle + 1, matrix_array.shape[0] - 1 - index_of_circle)):
         matrix_array[i, index_of_circle] = not_shifted[i + 1, index_of_circle]
 
+
+def get_matrix_shift(matrix_array: np.array, shift_times: int = 1):
+    result = matrix_array.copy()
+    print(np.min(matrix_array.shape) / 2)
+
+    for _ in range(0, shift_times):
+        for shift_index in range(0, int(np.min(matrix_array.shape) / 2)):
+            full_circle_shift_clockwise(result, shift_index)
+
+    return result
+
+
 if __name__ == '__main__':
-    matrix = np.arange(0, 6*8).reshape(6, 8)
+    matrix = np.arange(0, 8 * 10).reshape(8, 10)
     print(matrix)
+    print(get_matrix_shift(matrix))
 
-    full_circle_shift_clockwise(matrix, 0)
-
-
-    print(matrix)
