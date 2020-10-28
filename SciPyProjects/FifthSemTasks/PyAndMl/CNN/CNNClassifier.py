0231480f748507d52c361560e5a8323327125257
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-10-28 06:24:03 -0700

    Update CNNClassifier.py

--- SciPyProjects/FifthSemTasks/PyAndMl/CNN/CNNClassifier.py
+++ SciPyProjects/FifthSemTasks/PyAndMl/CNN/CNNClassifier.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+
 class CNNClassifier:
-    def convolution(self, image, filter_matrix, bias, s=1):
+    @staticmethod
+    def convolution(image, filter_matrix, bias, s=1):
         (n_f, n_c_f, f, _) = filter_matrix.shape  # filter dimensions
         n_c, in_dim, _ = image.shape  # image dimensions
 
@@ -21,11 +23,41 @@ class CNNClassifier:
                 # move filter horizontally across the image
                 while curr_x + f <= in_dim:
                     # perform the convolution operation and add the bias
-                    out[curr_f, out_y, out_x] = np.sum(filt[curr_f] * image[:, curr_y:curr_y + f, curr_x:curr_x + f]) + \
-                                                bias[curr_f]
+                    out[curr_f, out_y, out_x] = np.sum(filter_matrix[curr_f] *
+                                                       image[:, curr_y:curr_y + f, curr_x:curr_x + f]) + bias[curr_f]
                     curr_x += s
                     out_x += 1
                 curr_y += s
                 out_y += 1
 
         return out
+
+    @staticmethod
+    def maxpool(image, f=2, s=2):
+        """
+        Downsample input `image`using a kernel size of `f` and a stride of`s`
+        """
+        n_c, h_prev, w_prev = image.shape
+
+        # calculate output dimensions after the maxpooling operation.
+        h = int((h_prev - f) / s) + 1
+        w = int((w_prev - f) / s) + 1
+
+        # create a matrix to hold the values of the maxpooling operation.
+        downsampled = np.zeros((n_c, h, w))
+
+        # slide the window over every part of the image using stride s. Take the maximum value at each step.
+        for i in range(n_c):
+            curr_y = out_y = 0
+            # slide the max pooling window vertically across the image
+            while curr_y + f <= h_prev:
+                curr_x = out_x = 0
+                # slide the max pooling window horizontally across the image
+                while curr_x + f <= w_prev:
+                    # choose the maximum value within the window at each step and store it to the output matrix
+                    downsampled[i, out_y, out_x] = np.max(image[i, curr_y:curr_y + f, curr_x:curr_x + f])
+                    curr_x += s
+                    out_x += 1
+                curr_y += s
+                out_y += 1
+        return downsampled
