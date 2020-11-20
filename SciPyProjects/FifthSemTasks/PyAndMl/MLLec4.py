8674e15900552d91c31dc722156e21a31a000cc3
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-11-20 07:45:55 -0800

    add ml task4

---
+++ SciPyProjects/FifthSemTasks/PyAndMl/MLLec4.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+
+csv_data = pd.read_csv("Res\\IBM.csv", delimiter=';')
+
+csv_data.index = pd.to_datetime(csv_data['<DATE>'], format='%d.%m.%y')
+
+# part1
+
+val = csv_data.groupby(by=[csv_data.index.month])['<VOL>'].sum()
+print(val)
+val.plot(x='<DATE>', y='<VOL>', kind='bar')
+
+val = csv_data.groupby(by=[csv_data.index.month, csv_data.index.year])['<VOL>'].sum()
+print(val)
+val.plot(x='<DATE>', y='<VOL>', kind='bar')
+
+# part2
+
+val = csv_data.groupby(by=[csv_data.index.year])['<CLOSE>'].mean()
+print(val)
+val.plot(x='DATA', y='VOL', kind='bar')
