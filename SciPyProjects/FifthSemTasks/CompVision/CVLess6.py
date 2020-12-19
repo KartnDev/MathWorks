830e085f0a82850df634f513bd2144d6ffb64911
KartonDCP <dmutpuu2000@gmail.com>
2020-12-19 21:23:35 +0300

    final commit the CVLess6

--- SciPyProjects/FifthSemTasks/CompVision/CVLess6.py
+++ SciPyProjects/FifthSemTasks/CompVision/CVLess6.py
@@ -2,71 +2,23 @@ import cv2
 from PIL import ImageDraw, Image, ImageStat
 import matplotlib.pyplot as plt
 
-def halftone(im, cmyk, sample, scale):
-    """Returns list of half-tone images for cmyk image. sample (pixels),
-       determines the sample box size from the original image. The maximum
-       of possible dot sizes). So sample=1 will presevere the original image
-       output dot diameter is given by sample * scale (which is also the number
-       resolution, but scale must be >1 to allow variation in dot size."""
-    cmyk = cmyk.split()
-    dots = []
-    angle = 0
-    for channel in cmyk:
-        channel = channel.rotate(angle, expand=1)
-        size = channel.size[0] * scale, channel.size[1] * scale
-        half_tone = Image.new('L', size)
-        draw = ImageDraw.Draw(half_tone)
-        for x in range(0, channel.size[0], sample):
-            for y in range(0, channel.size[1], sample):
-                box = channel.crop((x, y, x + sample, y + sample))
-                stat = ImageStat.Stat(box)
-                diameter = (stat.mean[0] / 255) ** 0.5
-                edge = 0.5 * (1 - diameter)
-                x_pos, y_pos = (x + edge) * scale, (y + edge) * scale
-                box_edge = sample * diameter * scale
-                draw.ellipse((x_pos, y_pos, x_pos + box_edge, y_pos + box_edge), fill=255)
-        half_tone = half_tone.rotate(-angle, expand=1)
-        width_half, height_half = half_tone.size
-        xx = (width_half - im.size[0] * scale) / 2
-        yy = (height_half - im.size[1] * scale) / 2
-        half_tone = half_tone.crop((xx, yy, xx + im.size[0] * scale, yy + im.size[1] * scale))
-        dots.append(half_tone)
-        angle += 15
-    return dots
+
+def match_img_dist(img1, img2):
+    orb = cv2.ORB()
+
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    matches = bf.match(des1, des2)
+
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    return matches
 
 
-def gcr(im, percentage):
-    '''basic "Gray Component Replacement" function. Returns a CMYK image with
-       percentage gray component removed from the CMY channels and put in the
-       K channel, ie. for percentage=100, (41, 100, 255, 0) >> (0, 59, 214, 41)'''
-    cmyk_im = im.convert('CMYK')
-    if not percentage:
-        return cmyk_im
-    cmyk_im = cmyk_im.split()
-    cmyk = []
-    for i in range(4):
-        cmyk.append(cmyk_im[i].load())
-    for x in range(im.size[0]):
-        for y in range(im.size[1]):
-            gray = min(cmyk[0][x, y], cmyk[1][x, y], cmyk[2][x, y]) * percentage / 100
-            for i in range(3):
-                cmyk[i][x, y] = cmyk[i][x, y] - gray
-            cmyk[3][x, y] = gray
-    return Image.merge('CMYK', cmyk_im)
+img = cv2.imread('Resources//index.png', 0)
+scaled = cv2.resize(img, (0, 0), fx=0.2, fy=0.2)
 
-
-circle = cv2.imread('Resources//whiteCircle.png')
-
-cv2.imshow('Circle', circle)
-cv2.waitKey(0)
-
-
-cmyk_circle = gcr(circle, 0)
-dots_circle = halftone(circle, cmyk_circle, 10, 1)
-
-cv2.imshow('CircleHalftone', dots_circle)
-cv2.waitKey(0)
-
-ret, binary = cv2.threshold(dots_circle, 127, 255, cv2.THRESH_BINARY)
-
-cv2.batchDistance()
+print(match_img_dist(img, scaled))
