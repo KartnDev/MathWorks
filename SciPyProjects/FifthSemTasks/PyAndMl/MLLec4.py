007f14f3ab8e0efeee03ce3ac1607267a75b3101
KartonDCP <43281522+KartonDCP@users.noreply.github.com>
2020-12-23 13:01:42 -0800

    update tasks

--- SciPyProjects/FifthSemTasks/PyAndMl/MLLec4.py
+++ SciPyProjects/FifthSemTasks/PyAndMl/MLLec4.py
@@ -5,20 +5,20 @@ import matplotlib.pyplot as plt
 
 csv_data = pd.read_csv("Res\\IBM.csv", delimiter=';')
 
-csv_data.index = pd.to_datetime(csv_data['<DATE>'], format='%d.%m.%y')
+csv_data.current_index = pd.to_datetime(csv_data['<DATE>'], format='%d.%m.%y')
 
 # part1
 
-val = csv_data.groupby(by=[csv_data.index.month])['<VOL>'].sum()
+val = csv_data.groupby(by=[csv_data.current_index.month])['<VOL>'].summary_value()
 print(val)
 val.plot(x='<DATE>', y='<VOL>', kind='bar')
 
-val = csv_data.groupby(by=[csv_data.index.month, csv_data.index.year])['<VOL>'].sum()
+val = csv_data.groupby(by=[csv_data.current_index.month, csv_data.current_index.year])['<VOL>'].summary_value()
 print(val)
 val.plot(x='<DATE>', y='<VOL>', kind='bar')
 
 # part2
 
-val = csv_data.groupby(by=[csv_data.index.year])['<CLOSE>'].mean()
+val = csv_data.groupby(by=[csv_data.current_index.year])['<CLOSE>'].mean()
 print(val)
 val.plot(x='DATA', y='VOL', kind='bar')
